3a97ff8e81a56c0c3e5ed91f38881ac4be7f84ff
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-01-30 13:54:39 -0800

    finished the portfolio

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -11,30 +11,40 @@ export const projectsData = [
       image: Work1,
       title: "! Restaurant",
       category: "app",
+      link: "https://ifood-lime.vercel.app/",
+      github:"https://github.com/boymeetsblockchain/Restaurant-App"
     },
     {
       id: 2,
       image: Work2,
       title: "House Market",
       category: "web",
+      link: " https://house-market-project-six.vercel.app/",
+      github:"https://github.com/boymeetsblockchain/House-Market-Project"
     },
     {
       id: 3,
       image: Work3,
       title: "Broker site",
       category: "web",
+      link: "https://broker-site.vercel.app/",
+      github:"https://github.com/boymeetsblockchain/Broker-site"
     },
     {
       id: 4,
       image: Work4,
       title: "Hangman",
       category: "app",
+      link: "https://hangmanvaniilajs.netlify.app/",
+      github:"https://github.com/boymeetsblockchain/HangMan"
     },
     {
       id: 5,
       image: Work5,
       title: "Acealth",
       category: "design",
+      link: "https://acealth.netlify.app/",
+      github:"https://github.com/boymeetsblockchain/medical-website"
     },
   ];
   
--- src/components/work/WorksItems.jsx
+++ src/components/work/WorksItems.jsx
@@ -7,7 +7,10 @@ function WorksItems({
     <div className="work__card" key={item.id}>
        <img src={item.image} alt="" className='work__img' />
        <h3 className="work__title">{item.title}</h3>
-       <a href="#" className='work__button'> Demo
+       <a href={item.link} target='_blank' className='work__button'> Demo
+       <i className="bx bx-right-arrow-alt work__button-icon">
+        </i></a>
+        <a href={item.github} target='_blank'  className='work__button'>Github Repo
        <i className="bx bx-right-arrow-alt work__button-icon">
         </i></a>
     </div>
